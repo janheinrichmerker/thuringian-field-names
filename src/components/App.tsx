e41c4dcf65e750ba4401dda1da4942e360b79091
Jan Heinrich Reimer <heinrich@reimer.family>
2021-02-10 01:40:09 +0100

    Update routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import { Home } from "../routes/Home";
 import { Archive, Project } from "../routes/Project";
 import { Component } from "react";
@@ -8,15 +8,18 @@ import { AppFooter } from "./AppFooter";
 export class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <AppNav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/project" component={Project} />
+          <Route path="/archive">
+            <Redirect to="/project/archive" />
+          </Route>
           <Route exact path="/project/archive" component={Archive} />
         </Switch>
         <AppFooter />
-      </Router>
+      </HashRouter>
     );
   }
 }
