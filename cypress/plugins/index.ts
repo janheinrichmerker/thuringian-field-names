e1ec4386b52473f9d7606183e583aab539f31afe
Jan Heinrich Reimer <heinrich@reimer.family>
2021-02-11 16:51:11 +0100

    Update Cypress config

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,6 @@
 const config: Cypress.PluginConfig = (on, config) => {
   require("@cypress/react/plugins/react-scripts")(on, config);
+  require("@cypress/code-coverage/task")(on, config);
   return config;
 };
 
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,4 @@
 import "cypress-xpath";
 import "cypress-react-selector";
+import "@cypress/code-coverage/support";
 import "@cypress/react/support";
