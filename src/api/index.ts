5c7365a72cb99c40c8c598aa6908b8eff9e962c0
Jan Heinrich Reimer <heinrich@reimer.family>
2021-02-18 15:47:52 +0100

    Add API base config

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { API_USERNAME, API_PASSWORD } from "../secrets";
+
+export const api = axios.create({
+  baseURL: "https://collections.thulb.uni-jena.de/api/v1",
+  auth: {
+    username: API_USERNAME,
+    password: API_PASSWORD,
+  },
+});
