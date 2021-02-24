78603191d9710d9b6819869b55cfaa9081734787
Jan Heinrich Reimer <heinrich@reimer.family>
2021-02-24 23:30:26 +0100

    Add base model

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,3 +2,45 @@ export enum Loading {
   Idle,
   Pending,
 }
+
+export enum FieldNameType {
+  Marking, // Gemarkungen
+  Map, // Flurkarten
+}
+
+export interface Modification {
+  timestamp: Date;
+  author: string;
+}
+
+export interface GeoCoordinates {
+  latitude: number;
+  longitude: number;
+}
+
+export interface GeoArea {
+  from: GeoCoordinates;
+  to: GeoCoordinates;
+}
+
+export enum License {
+  CcByNcSa4,
+  Unknown,
+}
+
+export interface FieldNameSnippet {
+  id: string;
+  /**
+   * Identification number in DNB's "Gemeinsame Normdatei".
+   */
+  gndNumber: number;
+  title: string;
+  type: FieldNameType;
+  creation: Modification;
+  lastModification: Modification;
+}
+
+export interface FieldName extends FieldNameSnippet {
+  area: GeoArea;
+  license: License;
+}
