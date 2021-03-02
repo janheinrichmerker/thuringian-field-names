7b60077514de2b9e1321365c2b96d71f0cebfdc3
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-02 15:57:13 +0100

    Add 404 page

---
+++ src/routes/UnknownRoute.tsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default class UnknownRoute extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h2>Page not found</h2>
+            <p>
+              The page you requested could not be found. Do you want to go back
+              to the <Link to="/">homepage</Link>?
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
