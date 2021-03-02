366e629d5ce3cdc7b41533e7ad61bf17c77511d7
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-02 17:00:37 +0100

    Use forms components

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -5,6 +5,8 @@ import { connect, ConnectedProps } from "react-redux";
 import { AppDispatch, RootState } from "../store";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { login, register, selectUsersIsActive } from "../store/users";
+import LoginForm from "../components/LoginForm";
+import RegistrationForm from "../components/RegistrationForm";
 
 // Component props.
 interface Props {}
@@ -12,50 +14,6 @@ interface Props {}
 type CombinedProps = Props & ConnectedProps<typeof connector>;
 
 class User extends Component<CombinedProps> {
-  renderLogin() {
-    return (
-      <Form>
-        <Form.Group>
-          <Form.Label>Email or username</Form.Label>
-          <Form.Control type="email" placeholder="john.doe@example.com" />
-          <Form.Text className="text-muted">
-            You can log in using either your email address or your user name.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Sign in
-        </Button>
-      </Form>
-    );
-  }
-
-  renderSignup() {
-    return (
-      <Form.Group>
-        <Form.Group>
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="John Doe" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="john.doe@example.com" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Sign up
-        </Button>
-      </Form.Group>
-    );
-  }
-
   render() {
     if (this.props.isLoggedIn) {
       return <Redirect to="/" />;
@@ -78,10 +36,12 @@ class User extends Component<CombinedProps> {
               <Card.Body>
                 <Switch>
                   <Route exact path="/login">
-                    {this.renderLogin()}
+                    <LoginForm handleLogin={this.props.login} />
                   </Route>
                   <Route exact path="/signup">
-                    {this.renderSignup()}
+                    <RegistrationForm
+                      handleRegistration={this.props.register}
+                    />
                   </Route>
                 </Switch>
               </Card.Body>
