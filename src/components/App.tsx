fdcaaf44687ff58cf71c58f4f47af2ee3005c25c
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-03 02:33:46 +0100

    Add intl provider based on reducer locale

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,38 +8,50 @@ import { AppFooter } from "./AppFooter";
 import { UnknownRoute } from "../routes/UnknownRoute";
 import { User } from "../routes/User";
 import { Search } from "../routes/Search";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../store";
+import { selectSettingsLocale } from "../store/settings";
+import { IntlProvider } from "react-intl";
 
-export class App extends Component {
+class _App extends Component<ConnectedProps<typeof connector>> {
   render() {
     return (
-      <HashRouter>
-        <AppNav />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/project">
-            <Project />
-          </Route>
-          <Route path="/archive">
-            <Redirect to="/project/archive" />
-          </Route>
-          <Route exact path="/project/archive">
-            <Archive />
-          </Route>
-          <Route exact path="/login">
-            <User />
-          </Route>
-          <Route exact path="/signup">
-            <User />
-          </Route>
-          <Route path="/search/:id" component={Search} />
-          <Route>
-            <UnknownRoute />
-          </Route>
-        </Switch>
-        <AppFooter />
-      </HashRouter>
+      <IntlProvider locale={this.props.locale} key={this.props.locale}>
+        <HashRouter>
+          <AppNav />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/project">
+              <Project />
+            </Route>
+            <Route path="/archive">
+              <Redirect to="/project/archive" />
+            </Route>
+            <Route exact path="/project/archive">
+              <Archive />
+            </Route>
+            <Route exact path="/login">
+              <User />
+            </Route>
+            <Route exact path="/signup">
+              <User />
+            </Route>
+            <Route path="/search/:id" component={Search} />
+            <Route>
+              <UnknownRoute />
+            </Route>
+          </Switch>
+          <AppFooter />
+        </HashRouter>
+      </IntlProvider>
     );
   }
 }
+
+const connector = connect((state: RootState) => ({
+  locale: selectSettingsLocale(state),
+}));
+
+export const App = connector(_App);
