67d4d70664998c4bce3a07a9af820468539eaf82
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-03 03:44:00 +0100

    Move global initialization to index

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { Component, Fragment } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { Home } from "../routes/Home";
 import { Archive } from "../routes/Archive";
 import { Project } from "../routes/Project";
@@ -8,50 +8,38 @@ import { AppFooter } from "./AppFooter";
 import { UnknownRoute } from "../routes/UnknownRoute";
 import { User } from "../routes/User";
 import { Search } from "../routes/Search";
-import { connect, ConnectedProps } from "react-redux";
-import { RootState } from "../store";
-import { selectSettingsLocale } from "../store/settings";
-import { IntlProvider } from "react-intl";
 
-class _App extends Component<ConnectedProps<typeof connector>> {
+export class App extends Component {
   render() {
     return (
-      <IntlProvider locale={this.props.locale} key={this.props.locale}>
-        <HashRouter>
-          <AppNav />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/project">
-              <Project />
-            </Route>
-            <Route path="/archive">
-              <Redirect to="/project/archive" />
-            </Route>
-            <Route exact path="/project/archive">
-              <Archive />
-            </Route>
-            <Route exact path="/login">
-              <User />
-            </Route>
-            <Route exact path="/signup">
-              <User />
-            </Route>
-            <Route path="/search/:id" component={Search} />
-            <Route>
-              <UnknownRoute />
-            </Route>
-          </Switch>
-          <AppFooter />
-        </HashRouter>
-      </IntlProvider>
+      <Fragment>
+        <AppNav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/project">
+            <Project />
+          </Route>
+          <Route path="/archive">
+            <Redirect to="/project/archive" />
+          </Route>
+          <Route exact path="/project/archive">
+            <Archive />
+          </Route>
+          <Route exact path="/login">
+            <User />
+          </Route>
+          <Route exact path="/signup">
+            <User />
+          </Route>
+          <Route path="/search/:id" component={Search} />
+          <Route>
+            <UnknownRoute />
+          </Route>
+        </Switch>
+        <AppFooter />
+      </Fragment>
     );
   }
 }
-
-const connector = connect((state: RootState) => ({
-  locale: selectSettingsLocale(state),
-}));
-
-export const App = connector(_App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import { connect, ConnectedProps, Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { IntlProvider } from "react-intl";
 import { App } from "./components/App";
 import { reportWebVitals } from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import { RootState, store } from "./store";
+import { selectSettingsLocale } from "./store/settings";
+import en from "./translations/en.json";
+import de from "./translations/de.json";
+import "./index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function loadLocaleData(locale: string) {
+  switch (locale.split(/[-_]/)[0]) {
+    case "de":
+      return de;
+    default:
+      return en;
+  }
+}
+
+class Root extends Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <Provider store={store}>
+          <ConnectedRootApp />
+        </Provider>
+      </React.StrictMode>
+    );
+  }
+}
+
+class RootApp extends Component<ConnectedProps<typeof connector>> {
+  render() {
+    return (
+      <IntlProvider
+        locale={this.props.locale}
+        messages={loadLocaleData(this.props.locale)}
+        key={this.props.locale}
+      >
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </IntlProvider>
+    );
+  }
+}
+
+const connector = connect((state: RootState) => ({
+  locale: selectSettingsLocale(state),
+}));
+
+const ConnectedRootApp = connector(RootApp);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // To measure performance, pass a function to send to an analytics endpoint: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
