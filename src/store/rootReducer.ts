d01b4f2ef97e13d064c30d86ef2cc07735461a4b
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-03 02:27:40 +0100

    Add locale settings reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { usersReducer } from "./users";
 import { fieldNamesReducer } from "./fieldNames";
+import { settingsReducer } from "./settings";
 
 export const rootReducer = combineReducers({
   fieldNames: fieldNamesReducer,
   users: usersReducer,
+  settings: settingsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/store/settings.ts
@@ -0,0 +1,30 @@
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from ".";
+
+export interface SettingsState {
+  locale: string;
+}
+
+const initialState: SettingsState = {
+  locale: navigator.language,
+};
+
+const slice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    updateLocale: (state, action: PayloadAction<string>) => {
+      state.locale = action.payload;
+    },
+  },
+});
+
+export const { updateLocale } = slice.actions;
+
+export const selectSettings = (state: RootState) => state.settings;
+export const selectSettingsLocale = createSelector(
+  selectSettings,
+  (state) => state.locale
+);
+
+export const settingsReducer = slice.reducer;
