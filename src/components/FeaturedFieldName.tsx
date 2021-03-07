9a84cd6abeef69257c8353c0e9f977f0ef40946c
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 00:35:10 +0100

    Improve featured field name snippet

--- src/components/FeaturedFieldName.tsx
+++ src/components/FeaturedFieldName.tsx
@@ -2,31 +2,22 @@ import { Component } from "react";
 import { Card } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { FormattedDate } from "react-intl";
-import { FieldNameSnippet, FieldNameType } from "../model";
+import { FieldNameSnippet } from "../model";
+import { FormattedFieldNameType, GndLink } from ".";
 
 export class FeaturedFieldName extends Component<{
   snippet: FieldNameSnippet;
 }> {
-  typeToString(type: FieldNameType): string {
-    if (type === FieldNameType.Map) {
-      return "Map";
-    } else if (type === FieldNameType.Marking) {
-      return "Marking";
-    } else {
-      return "Unknown type";
-    }
-  }
-
   render() {
     return (
       <Card key={this.props.snippet.id}>
         <Card.Body>
           <Card.Title>{this.props.snippet.title}</Card.Title>
           <Card.Subtitle className="mb-2">
-            {this.typeToString(this.props.snippet.type)}
+            <FormattedFieldNameType type={this.props.snippet.type} />
           </Card.Subtitle>
           <Card.Text className="text-muted">
-            GND: {this.props.snippet.gndNumber}
+            GND number: <GndLink gndNumber={this.props.snippet.gndNumber} />
             <br />
             Last updated{" "}
             <FormattedDate value={this.props.snippet.lastModification.date} />
