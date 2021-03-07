dc188036575d4cc00ec7ccb6416abac4cf670517
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-07 19:39:38 +0100

    Improve search screen

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,9 +1,7 @@
 import { Component, Fragment } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { FormattedDate } from "react-intl";
 import { connect, ConnectedProps } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router-dom";
-import { FieldNameType } from "../model";
 import { AppDispatch, RootState } from "../store";
 import {
   selectSearchResults,
@@ -13,6 +11,8 @@ import {
 } from "../store/search";
 import { Search as SearchForm } from "../components/forms/Search";
 import { ApiErrorAlert } from "../components/ApiErrorAlert";
+import { SearchSnippets } from "../components/SearchSnippets";
+import { LoadingAlert } from "../components/LoadingAlert";
 
 interface Parameters {
   query: string;
@@ -31,62 +31,48 @@ class ConnectedSearch extends Component<
     }
   }
 
-  renderLoading() {
-    return <Fragment>Searching...</Fragment>;
-  }
-
-  renderList() {
-    return (
-      <Fragment>
-        Field names ({this.props.results.length}):
-        <ol>
-          {this.props.results.map((model) => (
-            <li key={model.id}>
-              <strong>{model.title}</strong>
-              <br />
-              {model.type === FieldNameType.Map
-                ? "Map"
-                : model.type === FieldNameType.Marking
-                ? "Marking"
-                : "Unknown type"}
-              <br />
-              Created <FormattedDate value={model.creation.date} /> by{" "}
-              {model.creation.author}
-              <details>
-                <summary>JSON</summary>
-                <pre>{JSON.stringify(model)}</pre>
-              </details>
-            </li>
-          ))}
-        </ol>
-      </Fragment>
-    );
-  }
-
   render() {
     console.log(this.props.location);
     return (
-      <Container key={this.props.location.pathname}>
-        <Row>
-          <Col>
-            <SearchForm
-              handleSearch={this.search.bind(this)}
-              query={this.props.match.params.query}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {this.props.loading ? (
-              this.renderLoading()
-            ) : this.props.error ? (
-              <ApiErrorAlert error={this.props.error} />
-            ) : (
-              this.renderList()
-            )}
-          </Col>
-        </Row>
-      </Container>
+      <Fragment>
+        <Container key={this.props.location.pathname}>
+          <Row>
+            <Col>
+              <SearchForm
+                handleSearch={this.search.bind(this)}
+                query={this.props.match.params.query}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>
+                {this.props.results.length} results for{" "}
+                <b>{this.props.match.params.query}</b>.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {this.props.loading ? (
+                <Row>
+                  <Col>
+                    <LoadingAlert />
+                  </Col>
+                </Row>
+              ) : this.props.error ? (
+                <Row>
+                  <Col>
+                    <ApiErrorAlert error={this.props.error} />
+                  </Col>
+                </Row>
+              ) : (
+                <SearchSnippets snippets={this.props.results} />
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     );
   }
 
