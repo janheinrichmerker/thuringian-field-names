e9f8ccda8c600e6d303ac4ffc77c27350e834916
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 00:55:54 +0100

    Implement data page

--- src/components/pages/DataPage.tsx
+++ src/components/pages/DataPage.tsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export class DataPage extends Component {
   render() {
@@ -7,10 +7,40 @@ export class DataPage extends Component {
       <Container>
         <Row>
           <Col>
-            <Alert variant="danger">
-              {/* TODO Implement component */}
-              Component not yet developed.
-            </Alert>
+            <h2>Datengrundlage</h2>
+            <p>
+              Idealerweise sollen von jedem Flurnamen folgende Punkte bekannt
+              sein:
+              <ul>
+                <li>
+                  die amtliche Namenform (aus ALK, Flurkarten und ‑büchern),
+                </li>
+                <li>die mundartliche Lautung des Namens,</li>
+                <li>
+                  historische (möglichst weit zurückreichende) Belege (z.B. aus
+                  historischen Karten, Lehnbriefen, Grenzbeschreibungen oder
+                  Gerichtsbüchern),
+                </li>
+                <li>
+                  die exakte Lage im Gelände (früher verbal beschrieben, heute
+                  mit Hilfe von GPS-Daten und den Karten des Thür.
+                  Landesvermessungsamtes exakter möglich),
+                </li>
+                <li>
+                  eine genaue Beschreibung des benannten Objektes (z.B. feuchte
+                  Wiese, steiniges Feld o.ä.),
+                </li>
+                <li>
+                  überlieferte Hinweise hinsichtlich des Namenursprungs (z.B.
+                  Sagen, Erzählungen),
+                </li>
+                <li>GND-Nummer der Gemarkung</li>
+              </ul>
+            </p>
+            <p>
+              Diese Angaben liefert das Thüringische Flurnamenarchiv jedoch nur
+              in Teilen, so dass weiterer Erhebungsbedarf besteht.
+            </p>
           </Col>
         </Row>
       </Container>
