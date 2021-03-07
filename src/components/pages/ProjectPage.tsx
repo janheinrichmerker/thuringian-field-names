f749a877f96938aa84c27aaf42b304a31d24736b
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 00:50:35 +0100

    Implement project page

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
 
 export class ProjectPage extends Component {
@@ -8,20 +9,19 @@ export class ProjectPage extends Component {
       <Container>
         <Row>
           <Col>
+            <h2>Das Projekt</h2>
+            <p>
+              <FormattedMessage id="project.longDescription" />
+            </p>
+            <p>
+              <FormattedMessage id="project.status" />
+            </p>
             <p>
               Want to learn more about the{" "}
               <Link to="/project/archive">archive</Link>,{" "}
               <Link to="/project/data">data</Link>, or{" "}
               <Link to="/project/data">our partners</Link>?
             </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Alert variant="danger">
-              {/* TODO Implement component */}
-              Component not yet developed.
-            </Alert>
           </Col>
         </Row>
       </Container>
