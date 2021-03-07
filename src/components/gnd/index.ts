6719442c40264565376e863c1e3bf4b41f3f3ff4
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-07 23:34:07 +0100

    Move GND components to subfolder

---
+++ src/components/gnd/index.ts
@@ -0,0 +1,4 @@
+// Re-export components to simplify importing.
+export { GndLink } from "./GndLink";
+export { GndLinkContainer } from "./GndLinkContainer";
+export { GndOverlay } from "./GndOverlay";
