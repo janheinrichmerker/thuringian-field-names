6aefb8969cb5965939762b2788c9e8cab1660319
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 01:09:53 +0100

    Implement partners page

--- src/components/pages/PartnersPage.tsx
+++ src/components/pages/PartnersPage.tsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export class PartnersPage extends Component {
   render() {
@@ -7,10 +7,104 @@ export class PartnersPage extends Component {
       <Container>
         <Row>
           <Col>
-            <Alert variant="danger">
-              {/* TODO Implement component */}
-              Component not yet developed.
-            </Alert>
+            <h2>Projektpartner</h2>
+            <p>
+              Partner des Thüringer Flurnamenprojektes sind:
+              <dl>
+                <dt>
+                  <a
+                    href="https://www.thulb.uni-jena.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Thüringer Universitäts- und Landesbibliothek
+                  </a>
+                </dt>
+                <dd>Entwicklung Datenbank und Portal</dd>
+                <dt>
+                  <a
+                    href="http://historische-kommission-fuer-thueringen.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Historische Kommission für Thüringen
+                  </a>
+                </dt>
+                <dd>
+                  wissenschaftliche Beratung hinsichtlich Thüringer
+                  Regionalgeschichte
+                </dd>
+                <dt>
+                  <a
+                    href="https://tlbg.thueringen.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Landesvermessungsamt Thüringen
+                  </a>
+                </dt>
+                <dd>Bereitstellung des digitalen Kartenmaterials</dd>
+                <dt>
+                  <a
+                    href="https://heimatbund-thueringen.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Heimatbund Thüringen e.V.
+                  </a>
+                </dt>
+                <dd>
+                  Bereitstellung der Sammlungen ehrenamtlicher Mitarbeiter im
+                  Projekt „Flurnamen und Regionalgeschichte“
+                </dd>
+                <dt>
+                  <a
+                    href="https://dhnet.uni-jena.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    DHnet | Jena
+                  </a>
+                </dt>
+                <dd>Beratung bei DH-relevanten Fragen</dd>
+                <dt>
+                  <a
+                    href="https://mscj.uni-jena.de"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Michael Stifel Center Jena
+                  </a>
+                </dt>
+                <dd>
+                  Beratung bei informationstechnologischen Fragestellungen
+                </dd>
+                <dt>
+                  <a href="https://gbv.de/" target="_blank" rel="noreferrer">
+                    Gemeinsamer Bibliotheksverbund (GBV
+                  </a>
+                  )
+                </dt>
+                <dt>
+                  <a
+                    href="https://denkmalpflege.thueringen.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Thüringer Landesamt für Archäologie
+                  </a>
+                </dt>
+                <dt>
+                  <a
+                    href="https://digicult-verbund.de/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    digiCULT Verbund eG
+                  </a>
+                </dt>
+              </dl>
+            </p>
           </Col>
         </Row>
       </Container>
