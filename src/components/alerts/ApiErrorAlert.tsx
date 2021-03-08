514abfa5c88499c5ec7c6e12993f286dea864692
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 23:56:34 +0100

    Migrate alerts from class to function components

--- src/components/alerts/ApiErrorAlert.tsx
+++ src/components/alerts/ApiErrorAlert.tsx
@@ -1,16 +1,15 @@
-import { Component } from "react";
+import { FunctionComponent } from "react";
 import { Alert } from "react-bootstrap";
 
 interface Props {
   error: string;
 }
-export class ApiErrorAlert extends Component<Props> {
-  render() {
-    return (
-      <Alert variant="warning">
-        <Alert.Heading>Error when requesting the API</Alert.Heading>
-        <p>{this.props.error}</p>
-      </Alert>
-    );
-  }
-}
+
+export const ApiErrorAlert: FunctionComponent<Props> = ({ error }) => {
+  return (
+    <Alert variant="warning">
+      <Alert.Heading>Error when requesting the API</Alert.Heading>
+      <p>{error}</p>
+    </Alert>
+  );
+};
--- src/components/alerts/LoadingAlert.tsx
+++ src/components/alerts/LoadingAlert.tsx
@@ -1,14 +1,12 @@
-import { Component } from "react";
+import { FunctionComponent } from "react";
 import { Alert, Spinner } from "react-bootstrap";
 
-export class LoadingAlert extends Component {
-  render() {
-    return (
-      <Alert variant="info">
-        <Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      </Alert>
-    );
-  }
-}
+export const LoadingAlert: FunctionComponent = () => {
+  return (
+    <Alert variant="info">
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </Alert>
+  );
+};
