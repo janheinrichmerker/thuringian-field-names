08d727f15c510625286565bd27c9bc05c98aab03
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 20:43:22 +0100

    Simplify field name type component

--- src/components/FormattedFieldNameType.tsx
+++ src/components/FormattedFieldNameType.tsx
@@ -1,26 +1,18 @@
 import { Component } from "react";
-import { injectIntl, WrappedComponentProps } from "react-intl";
+import { FormattedMessage } from "react-intl";
 import { FieldNameType } from "../model";
 
-class ConnectedFormattedFieldNameType extends Component<
-  WrappedComponentProps & {
-    type: FieldNameType;
-  }
-> {
+export class FormattedFieldNameType extends Component<{
+  type: FieldNameType;
+}> {
   render() {
     const type = this.props.type;
-    const formatMessage = this.props.intl.formatMessage;
-
     if (type === FieldNameType.Map) {
-      return formatMessage({ id: "fieldName.type.map" });
+      return <FormattedMessage id="fieldName.type.map" />;
     } else if (type === FieldNameType.Marking) {
-      return formatMessage({ id: "fieldName.type.marking" });
+      return <FormattedMessage id="fieldName.type.marking" />;
     } else {
-      return formatMessage({ id: "fieldName.type.unknown" });
+      return <FormattedMessage id="fieldName.type.unknown" />;
     }
   }
 }
-
-export const FormattedFieldNameType = injectIntl(
-  ConnectedFormattedFieldNameType
-);
