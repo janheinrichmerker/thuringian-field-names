9872d9413cebdcaa7e4e6cec91501267316fd276
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 20:37:57 +0100

    Add component for formatting licenses

---
+++ src/components/FormattedLicense.tsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { License } from "../model";
+
+export class FormattedLicense extends Component<{
+  license: License;
+}> {
+  render() {
+    const license = this.props.license;
+    if (license === License.CcByNcSa40) {
+      return (
+        <a
+          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FormattedMessage id="fieldName.license.ccByNcSa40" />
+        </a>
+      );
+    } else {
+      return <FormattedMessage id="fieldName.license.unknown" />;
+    }
+  }
+}
