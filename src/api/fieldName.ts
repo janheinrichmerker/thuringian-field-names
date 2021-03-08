3411816a8d53ceff289f66e167c2aee26acf05ee
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-08 19:04:40 +0100

    Add object API wrapper

---
+++ src/api/fieldName.ts
@@ -0,0 +1,25 @@
+import Api from ".";
+import { FieldName } from "../model";
+import { parseStringPromise } from "xml2js";
+import { WrappedMyCoreObject } from "./model";
+import { parseWrappedMyCoreObject } from "./parsers";
+
+export default class FieldNamesApi extends Api {
+  async getFieldName(id: string): Promise<FieldName> {
+    const result = await this.endpoint.get(`objects/${id}`, {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8",
+      },
+      responseType: "text",
+    });
+    const wrapped: WrappedMyCoreObject = await parseStringPromise(result.data, {
+      attrkey: "attributes",
+      charkey: "content",
+    });
+
+    const fieldName = parseWrappedMyCoreObject(wrapped);
+
+    console.log(fieldName.area);
+    return fieldName;
+  }
+}
