33a9f73df42aa20ced6d6d991384cab168108e3a
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-10 10:04:46 +0100

    Remove redundant import

--- src/components/pages/SubmitPage.tsx
+++ src/components/pages/SubmitPage.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { SubmitForm } from "..";
 
 export const SubmitPage: FunctionComponent = () => {
