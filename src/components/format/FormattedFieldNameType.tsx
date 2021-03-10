daa7a0ffa356d0dbd4dcb94c67734a551b2c8b65
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-10 13:33:14 +0100

    Simplify formatters

--- src/components/format/FormattedFieldNameType.tsx
+++ src/components/format/FormattedFieldNameType.tsx
@@ -1,15 +1,11 @@
 import { FunctionComponent } from "react";
 import { FormattedMessage } from "react-intl";
 import { FieldNameType } from "../../model";
+import { getFieldNameTypeMessageDescriptor } from "../../utils";
 
 export const FormattedFieldNameType: FunctionComponent<{
   type: FieldNameType;
 }> = ({ type }) => {
-  if (type === FieldNameType.Map) {
-    return <FormattedMessage id="fieldName.type.map" />;
-  } else if (type === FieldNameType.Marking) {
-    return <FormattedMessage id="fieldName.type.marking" />;
-  } else {
-    return <FormattedMessage id="fieldName.type.unknown" />;
-  }
+  const descriptor = getFieldNameTypeMessageDescriptor(type);
+  return <FormattedMessage id={descriptor.id} />;
 };
--- src/components/format/FormattedLicense.tsx
+++ src/components/format/FormattedLicense.tsx
@@ -1,21 +1,19 @@
 import { FunctionComponent } from "react";
 import { FormattedMessage } from "react-intl";
 import { License } from "../../model";
+import { getLicenseMessageDescriptor } from "../../utils";
 
 export const FormattedLicense: FunctionComponent<{
   license: License;
 }> = ({ license }) => {
-  if (license === License.CcByNcSa40) {
+  const descriptor = getLicenseMessageDescriptor(license);
+  if (descriptor.url) {
     return (
-      <a
-        href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FormattedMessage id="fieldName.license.ccByNcSa40" />
+      <a href={descriptor.url} target="_blank" rel="noreferrer">
+        <FormattedMessage id={descriptor.id} />
       </a>
     );
   } else {
-    return <FormattedMessage id="fieldName.license.unknown" />;
+    return <FormattedMessage id={descriptor.id} />;
   }
 };
