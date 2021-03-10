f94458f56c96ef9d4f416d7d4805020b9479c090
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-10 03:07:09 +0100

    Implement visual submit page draft

--- src/components/pages/SubmitPage.tsx
+++ src/components/pages/SubmitPage.tsx
@@ -1,17 +1,22 @@
 import { FunctionComponent } from "react";
 import { Container, Row, Col, Alert } from "react-bootstrap";
+import { SubmitForm } from "..";
 
 export const SubmitPage: FunctionComponent = () => {
   return (
     <Container>
       <Row>
         <Col>
-          <Alert variant="danger">
-            {/* TODO Implement component */}
-            Component not yet developed.
-          </Alert>
+          <h2>Submit a new field name</h2>
+          <p>
+            Using this form, you can contribute to the Thuringian Field Names
+            Archive. If you know a field name and have verified that the field
+            name not yet exists please submit a new name. We will check your
+            contribution and publish it if it meets our quality requirements.
+          </p>
         </Col>
       </Row>
+      <SubmitForm submit={() => {}} loading={false} />
     </Container>
   );
 };
