c332510c183b7b686db0498175b3c3bab99b5f5c
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-10 13:32:58 +0100

    Add field name type / license formatting utils

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+// Re-export to simplify importing.
+export {
+  getFieldNameTypeMessageDescriptor,
+  formatFieldNameType,
+  getLicenseMessageDescriptor,
+  formatLicense,
+} from "./intl";
---
+++ src/utils/intl.ts
@@ -0,0 +1,40 @@
+import { IntlShape, MessageDescriptor } from "react-intl";
+import { FieldNameType, License } from "../model";
+
+export interface FieldNameTypeMessageDescriptor extends MessageDescriptor {
+  id: string | number;
+}
+
+export function getFieldNameTypeMessageDescriptor(type: FieldNameType) {
+  if (type === FieldNameType.Map) {
+    return { id: "fieldName.type.map" };
+  } else if (type === FieldNameType.Marking) {
+    return { id: "fieldName.type.marking" };
+  } else {
+    return { id: "fieldName.type.unknown" };
+  }
+}
+
+export function formatFieldNameType(intl: IntlShape, type: FieldNameType) {
+  return intl.formatMessage(getFieldNameTypeMessageDescriptor(type));
+}
+
+export interface LicenseMessageDescriptor extends MessageDescriptor {
+  id: string | number;
+  url?: string;
+}
+
+export function getLicenseMessageDescriptor(license: License) {
+  if (license === License.CcByNcSa40) {
+    return {
+      id: "fieldName.license.ccByNcSa40",
+      url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+    };
+  } else {
+    return { id: "fieldName.license.unknown" };
+  }
+}
+
+export function formatLicense(intl: IntlShape, license: License) {
+  return intl.formatMessage(getLicenseMessageDescriptor(license));
+}
