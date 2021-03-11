e4df72240c315151db99175958e42a4d8318d672
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-11 15:47:09 +0100

    Fix Cypress nesting

--- cypress/integration/structure.ts
+++ cypress/integration/structure.ts
@@ -8,21 +8,22 @@ context("Structure", () => {
   });
 
   it("contains project name", () => {
-    cy.get("#root > nav.AppNav")
-      .get(".navbar-brand")
-      .contains(/Thuringian Field Names/)
-      .should("have.length", 1);
+    cy.get("#root > nav.AppNav").within(() => {
+      cy.get(".navbar-brand")
+        .contains(/Thuringian Field Names/i)
+        .should("have.length", 1);
+    });
   });
 
   it("has search bar", () => {
-    cy.get("#root > nav.AppNav")
-      .get("form")
-      .within(() => {
+    cy.get("#root > nav.AppNav").within(() => {
+      cy.get("form").within(() => {
         cy.get("input")
           .invoke("attr", "placeholder")
-          .should("match", /.*Search.*/);
+          .should("match", /Search/i);
         cy.get("svg.fa-search").should("have.length", 1);
       });
+    });
   });
 
   it("has exactly one content container", () => {
@@ -34,78 +35,83 @@ context("Structure", () => {
   });
 
   it("contains copyright information", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".navbar-text")
-      .contains(/Copyright/)
-      .should("have.length", 1);
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".navbar-text")
+        .contains(/Copyright/i)
+        .should("have.length", 1);
+    });
   });
 
   it("has imprint link", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/Imprint/)
-      .should("have.length", 1);
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/Imprint/i)
+        .should("have.length", 1);
+    });
   });
 
   it("has privacy policy link", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/Privacy/)
-      .should("have.length", 1);
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/Privacy/i)
+        .should("have.length", 1);
+    });
   });
 
   it("has language selector", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/Language/)
-      .should("have.length", 1);
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/Language/i)
+        .should("have.length", 1);
+    });
   });
 
   it("has language links", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/Language/)
-      .should("have.length", 1)
-      .click()
-      .parent()
-      .get(".dropdown-menu")
-      .within(() => {
-        cy.get(".dropdown-item")
-          .contains(/English/)
-          .should("have.length", 1);
-        cy.get(".dropdown-item")
-          .contains(/German/)
-          .should("have.length", 1);
-      });
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/Language/i)
+        .should("have.length", 1)
+        .click()
+        .siblings(".dropdown-menu")
+        .within(() => {
+          cy.get(".dropdown-item")
+            .contains(/English/i)
+            .should("have.length", 1);
+          cy.get(".dropdown-item")
+            .contains(/German/i)
+            .should("have.length", 1);
+        });
+    });
   });
 
   it("selected language is English", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/Language/)
-      .click()
-      .parent()
-      .get(".dropdown-menu")
-      .within(() => {
-        cy.get(".dropdown-item")
-          .contains(/English/)
-          .should("have.class", "active");
-        cy.get(".dropdown-item")
-          .contains(/German/)
-          .should("not.have.class", "active");
-      });
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/Language/i)
+        .click()
+        .siblings(".dropdown-menu")
+        .within(() => {
+          cy.get(".dropdown-item")
+            .contains(/English/i)
+            .should("have.class", "active");
+          cy.get(".dropdown-item")
+            .contains(/German/i)
+            .should("not.have.class", "active");
+        });
+    });
   });
 
   it("has source code link", () => {
-    cy.get("#root > nav.AppFooter")
-      .get(".nav-link")
-      .contains(/GitHub/)
-      .should("have.length", 1)
-      .should(
-        "have.attr",
-        "href",
-        "https://github.com/heinrichreimer/thuringian-field-names/"
-      );
+    cy.get("#root > nav.AppFooter").within(() => {
+      cy.get(".nav-link")
+        .contains(/GitHub/i)
+        .should("have.length", 1)
+        .should(
+          "have.attr",
+          "href",
+          "https://github.com/heinrichreimer/thuringian-field-names/"
+        );
+    });
   });
 });
 
